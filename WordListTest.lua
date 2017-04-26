ae5ef5a500c7aca29d44151fcc11e0774be9c827
brendonsida <brendonsida@u.boisestate.edu>
2017-04-25 23:46:34 -0600

    changed parse to work with Wordlist

--- WordListTest.lua
+++ WordListTest.lua
@@ -1,12 +1,13 @@
 --WordList test
 require("WordList")
+require("parse")
 io.write("starting...\n")
 list = WordList
 WordList.add(list, "hello")
 WordList.printList(list)
 WordList.add(list, "hello")
 WordList.printList(list)
-
+parse("text.txt",list)
 if WordList.contains(list, "hello") then
 	print("WOOHOO")
 else
